56c4fcd976cf59f88871c6b33c33bfee0cef376a
Yasin Javar <yasinjjjv@gmail.com>
2021-02-06 12:22:13 +0330

    For Deploy to Heroku

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=t10kuujii902+wyox+)nbt*&0#j-lj%mhf4&ri%qrhb^m0^m8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
